8963b6df68bcfec3c931ebe78af0dc1f4fb231d8
Filip Ivić <filipivic@Filips-MacBook-Air.local>
2020-11-03 18:42:04 +0100

    Adding Logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import NavigationHeader from '../components/NavigationHeader'
 const IndexPage = () => (
   <main>
     <ContactBar />
+    <NavigationHeader />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>This is were we will add content</p>
