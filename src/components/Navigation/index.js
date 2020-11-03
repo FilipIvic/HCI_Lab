e02e6cb658c2e731d602f31e884edb28a81cd6a0
Filip Ivić <filipivic@Filips-MacBook-Air.local>
2020-11-03 19:01:32 +0100

    adding NavigationBar part 1

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import styles from './style.module.css'
 
+import NavigationBar from '../NavigationBar'
+import Combobox from '../Combobox'
+
 const Navigation = () => (
   <section>
     <NavigationBar />
