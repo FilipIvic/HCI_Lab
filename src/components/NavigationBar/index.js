98a1a1dd7f5ac9640292b5c88758dd246a1ae5d5
Filip Ivić <filipivic@Filips-MacBook-Air.local>
2020-11-03 19:19:06 +0100

    refactor tabs to use map()

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -2,13 +2,57 @@ import React from 'react'
 
 import styles from './style.module.css'
 
+const navTabs = ['Home', 'Accommodation', 'Photo Gallery', 'Contact']
+
 const NavigationBar = () => (
-  <nav className={styles.navigationBar}>
+    <nav className={styles.navigationBar}>
+      {navTabs.map(tab => {
+      if (tab === 'Home')
+        return (
+          <li className={styles.active}>{tab}</li>)
+      else
+        return <li>{tab}</li>
+      }
+    )}
+    </nav>
+  )
+
+export default NavigationBar
+
+/*
+      #hard-codirana komponenta
+
       <li className={styles.active}>Home</li>
       <li>Accommodation</li>
       <li>Photo gallery</li>
       <li>Contact</li>
-  </nav>
-)
 
-export default NavigationBar
+      #nema styla active
+
+      {navTabs.map(
+        tab => <li>{tab}</li>
+      )}
+
+      može se trenutno zapisat i ovako:
+
+      const NavigationBar = () => (
+        <nav className={styles.navigationBar}>
+          {navTabs.map(tab => <li className={tab==='Home' ? styles.active : ''}> {tab} </li>)}
+        </nav>
+      )
+
+      #iza strijelica se vraca odijeljak (unutar kojeg se nalaze html tagovi, a ako zelimo vratiti i kod
+      onda stavljamo {array.map( el => )}) 
+
+      unutar maps funkcije, ako imamo:
+      1) jednu liniju mozemo pisat normalno;
+         el => <li>{tab}</li>
+      2) više linija 
+        el => {
+          if (tab === 'Home')
+            return (<li className={styles.active}> {tab} </li>)
+          else
+            return <li> {tab} </li>
+        }
+
+*/
