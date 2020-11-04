bdb511a50db9e7d637e69c7ac600d557c4c64b11
Filip Ivić <filipivic@Filips-MacBook-Air.local>
2020-11-04 21:01:35 +0100

    commit 8

--- src/components/ImageParagraph/index.js
+++ src/components/ImageParagraph/index.js
@@ -2,7 +2,9 @@ import React from 'react'
 import styles from './style.module.css'
  
 const ImageParagraph = () => <section className={styles.imageParagraph}>
- 
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>This is were we will add content</p>
 </section> 
  
 export default ImageParagraph
