bbf502f4f3d8939a690fc9e896796e9fda83a9aa
Filip Ivić <filipivic@Filips-MacBook-Air.local>
2020-11-04 20:41:23 +0100

    commit 5

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
  
 import HeaderFooterLayout from "../layouts/headerFooter"
+import TitlePage from '../modules/TitlePage'
  
 const IndexPage = () => (
   <HeaderFooterLayout activeTab="Home">
     {/* Body components here! */}
+    <TitlePage />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>This is were we will add content</p>
