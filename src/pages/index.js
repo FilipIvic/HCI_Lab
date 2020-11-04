de059030a27bfaf8e1bfac174afca2651dbed14c
Filip Ivić <filipivic@Filips-MacBook-Air.local>
2020-11-04 18:56:05 +0100

    Adding ContactBar to NavigationHeader

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
 import React from "react"
-import ContactBar from '../components/contactBar'
+
 import NavigationHeader from '../components/NavigationHeader'
 import Footer from '../components/Footer'
 
 const IndexPage = () => (
   <main>
-    <ContactBar />
     <NavigationHeader activeTab = "Contact"/>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
